19c2ebe789cf70db264c8b7bc6c46bf31ed8d27b
huss2012 <huss24940@gmail.com>
2024-04-12 08:37:15 +0300

    Second problem has been solved.

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,7 +1,20 @@
+const { expect } = require('chai');
 const Person = require('./person');
 
 // Your code here
+class Teacher extends Person{
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName, subject, yearsOfExperience)
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
 
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((crru, iv) => {
+      return crru += iv.yearsOfExperience;
+    }, 0)
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
